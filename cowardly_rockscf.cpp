aaa384fc72a438bd5cccb4c4545b91cec58802b2
ritiksharma0220 <76057415+ritiksharma0220@users.noreply.github.com>
2023-01-09 13:14:55 +0530

    Add files via upload

---
+++ cowardly_rockscf.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ss string
+ll i,j,k,t,flag;
+int main(){
+    cin>>t;
+    while (t--)
+    {
+        ll m,n;
+        cin>>n>>m;
+        for ( i = 0; i < m; i++)
+        {
+            ll x,y;
+            cin>>x>>y;
+        }
+        if (m==n)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+        
+    }
+    
+}
