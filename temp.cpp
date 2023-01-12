9d26be6305b89a7318858bce96bb0185ec5b8903
ritiksharma0220 <76057415+ritiksharma0220@users.noreply.github.com>
2023-01-12 23:56:45 +0530

    Add files via upload

--- temp.cpp
+++ temp.cpp
@@ -4,30 +4,23 @@ using namespace std;
 #define ss string
 ll i,j,k,t,flag;
 int main(){
-    cin>>t;
-    while (t--)
+    ll q;
+    cin>>q;
+    while (q--)
     {
-        ll n,k,x;
-        map<ll, ll> m;
+        ll a[4];
         for ( i = 0; i < n; i++)
         {
-            cin>>x;
-            if (m.find(i%k!=m.end()))
-            {
-                m[i%k]=max(m[i%k],x);
-                
-            }
-            else
-                {
-                    m[i%k]=x;
-                }
+            cin>>a[i];
         }
-            flag=0;
-            for (auto it:m)
-            {
-                ans+=it.second();
-            }
-            cout<<ans<<endl;
+        ll a1,a2,a4;
+        a1=max(a[2],a[3]);
+        a4=min(a[2],a[3]);
+        a2=max(a[0],a[1]);
+        if (a4>a2||)
+        {
+            /* code */
+        }
         
     }
     
